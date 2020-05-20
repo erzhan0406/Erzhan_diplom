a66fe1d9395d63e171ff520047e2a1004af7eb0d
air <asilkhan-al@mail.ru>
2020-05-20 14:20:31 +0600

    Diploma

--- resources/views/kazakhstan.blade.php
+++ resources/views/kazakhstan.blade.php
@@ -12,7 +12,7 @@
                             <i class="fa fa-home"></i>
                         </a>
                     </li>
-                    <li><span>Users</span></li>
+                    <li><span>Мониторинг</span></li>
                 </ol>
 
                 <a class="sidebar-right-toggle"><i class="fa fa-chevron-left"></i></a>
@@ -25,11 +25,11 @@
                 <div class="row justify-content-center">
                     <div class="col-md-12">
                         <div class="card">
-                            <div class="card-header">Анализ</div>
+                            <div class="card-header">Мониторинг</div>
 
                             <div class="card-body">
 
-                                <h2>Алматы облысы бойынша жер сілкіністері жайлы анализ</h2>
+                                <h2>Алматы облысы бойынша жер сілкіністерінің мониторингі</h2>
 
                                 <div class="embed-responsive embed-responsive-16by9">
                                     <iframe class="embed-responsive-item"
